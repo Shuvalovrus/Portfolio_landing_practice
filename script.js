470620cf40a0734d0d60bc7fdd041c86d893a406
Konstantin Shuvalov <shuvalov.2021@list.ru>
2022-04-11 11:23:08 +0300

    fix: change burger menu

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const iconMenu = document.querySelector('.icon-menu');
 const menuBody = document.querySelector('.menu');
+
 if (iconMenu) {
     iconMenu.addEventListener('click', (e) => {
         document.body.classList.toggle('_lock');
